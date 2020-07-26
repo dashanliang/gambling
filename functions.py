32e17174ca5a0816e8978601088a3062c977885b
dashanliang <dashan.liang@airwallex.com>
2020-07-26 20:53:11 +0800

    add functions

--- functions.py
+++ functions.py
@@ -1,20 +1,61 @@
-# get the value of state s, when action is a
-
-def srpossibilityonsa(s, r, sa, a):
-    possibility = 0.0
-    if s == sa + a and r == a:
-        possibility = 0.4
-    if s == sa - a and r == a:
-        possibility = 0.6
-
-    return possibility
+import numpy as np
+import copy
 
 
-def eachvalueona(s, a):
-    r = [a, -a]
+
+
+def getValueByState(s, valuedatas):
+    tmps = s -1
+    # print(tmps)
+    if tmps < 0:
+        return 0
+    if tmps == 99:
+        return 1
+    return valuedatas[s-1]
+
+def eachvalueona(s, a, valuedatas):
     s_next = [s+ a, s-a]
+    p = [0.4, 0.6]
     data = 0
     for i in [0, 1]:
-        data = data + srpossibilityonsa(s_next[i], r[i], s, a)
-
+        data = data + p[i] * (0 + getValueByState(s_next[i], valuedatas))
     return data
+
+def getactionsfors(s):
+    if s <= 100 -s:
+        return np.arange(1, s+1, 1)
+    else:
+        return np.arange(1, 101-s, 1)
+
+def getmaxdata(s, valuedatas, maxactions):
+    actionsforcal = getactionsfors(s)
+    tmp = 0
+    action = 0
+    for a in actionsforcal:
+        if tmp <= eachvalueona(s, a, valuedatas):
+            action = a
+            tmp = eachvalueona(s, a, valuedatas)
+    maxactions[s-1] = action
+    print(tmp)
+    return tmp
+
+
+if __name__ == '__main__':
+    valuedatas = np.zeros(99)
+    # valuedatas[99] = 1
+    actions = np.zeros(99)
+    tmpvalues = np.zeros(99)
+    tmpvalues = copy.deepcopy(valuedatas)
+    # print(valuedatas)
+    for j in np.arange(0, 200, 1):
+        for i in np.arange(1, 100, 1):
+            tmpvalues[i-1] = getmaxdata(i, valuedatas, actions)
+        valuedatas = copy.deepcopy(tmpvalues)
+    print(valuedatas)
+    print(actions)
+
+    # for i in np.arange(1, 100, 1):
+    #     tmpvalues[i-1] = getmaxdata(i, valuedatas, actions)
+    # valuedatas = copy.deepcopy(tmpvalues)
+    # print(valuedatas)
+        # print(actions)
